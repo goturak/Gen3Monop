60a958bd367d1437deb5951374c6428ba299615d
Claude-André <claudeuw@hotmail.com>
2019-04-05 16:09:41 +0200

    add Piece and Player with getName AND COMMENTS

--- src/Game/Piece.java
+++ src/Game/Piece.java
@@ -1,12 +1,24 @@
 package Game;
 
+/**
+ * class representing a piece in a monopoly game
+ * @author @author Guillaume Vetter & Luca Reis de Carvalho & Claude-André Alves
+ */
 public class Piece {
     private String name;
 
+    /**
+     * Piece Constructor
+     * @param name Piece name
+     */
     public Piece(String name) {
         this.name = name;
     }
 
+    /**
+     * Name getter
+     * @return Piece name
+     */
     public String getName() {
         return name;
     }
