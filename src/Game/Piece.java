5a507a3d57c8cfbe0717e77f5252340c226aea17
Claude-André <claudeuw@hotmail.com>
2019-04-05 16:00:36 +0200

    add Piece and Player with getName

---
+++ src/Game/Piece.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Piece {
+    private String name;
+
+    public Piece(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
