8f7da32bd177b29a00ccac8e402daed460e11fc5
Claude-André <claudeuw@hotmail.com>
2019-05-05 17:26:23 +0200

    more complex constructor of player

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -2,6 +2,8 @@ package Player;
 
 import Game.*;
 
+import java.util.ArrayList;
+
 /**
  * class representing a player in a monopoly game
  * @author @author Guillaume Vetter & Luca Reis de Carvalho & Claude-André Alves
@@ -9,12 +11,20 @@ import Game.*;
 public class Player {
     private String name;
     private Piece piece;
+    private Board board;
+    private ArrayList<Die> dice;
 
+    public Player(String name) {
+        this.name = name;
+    }
     /**
      * Player constructor
      * @param name Player name
      */
-    public Player(String name) {
+    public Player(String name, String piece, Board board, ArrayList<Die> dice) {
+        this.piece = new Piece(piece, board.getSquare(0));
+        this.board = board;
+        this.dice = dice;
         this.name = name;
     }
 
@@ -32,5 +42,9 @@ public class Player {
     public void takeTurn() {
         Die d1 = new Die();
         Die d2 = new Die();
+        Board b = new Board();
+
+        d1.roll();
+        d2.roll();
     }
 }
